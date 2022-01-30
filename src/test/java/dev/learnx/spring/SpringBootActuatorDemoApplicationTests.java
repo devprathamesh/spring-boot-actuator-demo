f0904999605a7bd6e23bda32baf5c3b76c6b1afd
Prathamesh Bandivadekar <bandivadekar.prathamesh@gmail.com>
2022-01-30 23:28:14 +0530

    Checkin for Create a sample project structure with minimal dependencies, closes #1

---
+++ src/test/java/dev/learnx/spring/SpringBootActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.learnx.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
